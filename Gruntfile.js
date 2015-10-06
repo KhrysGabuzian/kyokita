b2cfef5369ba52fb992b8c97b70a1c334a4f1819
Khrystyna <kgabuzian@wemanity.com>
2015-10-06 15:51:26 +0200

    compass deleted autoprefixer installed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,45 @@
 module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('node_modules/grunt/package.json'),
+
         sass: {
             dist: {
-                options: {
-                    style: 'compressed'
-                },
+                /*options:{
+                    style:'compressed'
+                },*/
                 files: {
                     'css/style.css' : 'scss/style.scss'
                 }
             }
         },
-        autoprefixer: {
-            dist: {
-                files: {
-                    'css/style.css': 'css/style.css'
+        autoprefixer:{
+            options: {
+                browsers: ['last 4 versions']
+            },
+            single_file: {
+                src: 'css/style.css',
+                dest: 'css/style.css'
+            }/*,
+            dist:{
+                files:{
+                    'css/style.css':'css/style.css'
                 }
-            }
+            }*/
         },
         watch: {
             css: {
-                files: '**/*.scss',
-                tasks: ['sass']
+                files: 'scss/*.scss',
+                tasks: ['sass', 'autoprefixer']
             }
         }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-
-    grunt.registerTask('default', ['concat', 'uglify' , 'compass', 'sass',  'imagemin']);
     grunt.registerTask('default',['watch']);
-}
+};
